f81e4f48e9c5c13728c435ae2a4b5924d2665bfb
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 09:25:54 +0000

    Fri Sep 28 09:25:54 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,46 +3,47 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 	public final String FIZZ = "fizz";
 	public final String BUZZ = "buzz";
-	public final String FIZZBUZZ = FIZZ +" " + BUZZ;
+	public final String FIZZBUZZ = FIZZ + " " + BUZZ;
 
 	public String fizzBuzz(Integer number) {
 		return doFizzBuzzLogic(number);
 	}
-	public String doFizzBuzzWith3or5NumberCheck(Integer number){
+
+	public String doFizzBuzzWith3or5NumberCheck(Integer number) {
 		return doFizzBuzzLogicWith3or5NumberCheck(number);
 	}
 
 	private String doFizzBuzzLogic(Integer number) {
 		if (number % 3 == 0) {
 			if (number % 15 == 0) {
-				//if divisible by 3 and 5 return FIZZBUZZ
+				// if divisible by 3 and 5 return FIZZBUZZ
 				return FIZZBUZZ;
 			} else {
-				//if divisible 3 only return FIZZ
+				// if divisible 3 only return FIZZ
 				return FIZZ;
 			}
 		} else if (number % 5 == 0) {
-			//if divisible 5 only return BUZZ
+			// if divisible 5 only return BUZZ
 			return BUZZ;
 		}
 		return String.valueOf(number);
 	}
+
 	private String doFizzBuzzLogicWith3or5NumberCheck(Integer number) {
 		String numberToCheck = String.valueOf(number);
 		String number3 = String.valueOf(3);
 		String number5 = String.valueOf(5);
-		
-		
+
 		if (number % 3 == 0) {
-			if (number % 15 == 0 || numberToCheck.toString().contains(number3)) {
-				//if divisible by 3 and 5 return FIZZBUZZ
-				return FIZZBUZZ;
+			if (number % 3 == 0 || numberToCheck.toString().contains(number3)) {
+				// if divisible by 3 or contains a 3
+				return FIZZ;
 			} else {
-				//if divisible 3 only return FIZZ
+				// if divisible 3 only return FIZZ
 				return FIZZ;
 			}
-		} else if (number % 5 == 0) {
-			//if divisible 5 only return BUZZ
+		} else if (number % 5 == 0 || numberToCheck.toString().contains(number5)) {
+			// if divisible 5 only return BUZZ
 			return BUZZ;
 		}
 		return String.valueOf(number);
