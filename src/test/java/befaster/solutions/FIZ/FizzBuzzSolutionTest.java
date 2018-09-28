93f5558b491526a46c648844bd24e75196ceadc1
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 08:18:34 +0000

    Fri Sep 28 08:18:34 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FizzBuzzSolutionTest {
-
+	FizzBuzzSolution fizzBuzzSolution = new FizzBuzzSolution();
 	@Test
-	public void testFizzBuzz() {
-		fail("Not yet implemented");
+	public void testSolutionForFizz() {
+		assertEquals(fizzBuzzSolution.FIZZ, fizzBuzzSolution.fizzBuzz(3));
+	}
+	@Test
+	public void testSolutionForBuzz(){
+		assertEquals(fizzBuzzSolution.BUZZ,fizzBuzzSolution.fizzBuzz(5));
 	}
 
 }
