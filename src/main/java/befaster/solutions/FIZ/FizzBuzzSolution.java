4cbebf5548015af6187f3904cd587b6b387e3d3f
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 09:13:55 +0000

    Fri Sep 28 09:13:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -8,8 +8,26 @@ public class FizzBuzzSolution {
 	public String fizzBuzz(Integer number) {
 		return doFizzBuzzLogic(number);
 	}
+	public String doFizzBuzzWith3or5NumberCheck(Integer number){
+		return doFizzBuzzLogicWith3or5NumberCheck(number);
+	}
 
 	private String doFizzBuzzLogic(Integer number) {
+		if (number % 3 == 0) {
+			if (number % 15 == 0) {
+				//if divisible by 3 and 5 return FIZZBUZZ
+				return FIZZBUZZ;
+			} else {
+				//if divisible 3 only return FIZZ
+				return FIZZ;
+			}
+		} else if (number % 5 == 0) {
+			//if divisible 5 only return BUZZ
+			return BUZZ;
+		}
+		return String.valueOf(number);
+	}
+	private String doFizzBuzzLogicWith3or5NumberCheck(Integer number) {
 		if (number % 3 == 0) {
 			if (number % 15 == 0) {
 				//if divisible by 3 and 5 return FIZZBUZZ
