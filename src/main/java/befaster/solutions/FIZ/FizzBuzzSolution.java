9ca5014eb3acc054ebf252385f29401e44d04650
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 08:15:34 +0000

    Fri Sep 28 08:15:34 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,7 +1,5 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 	public final String FIZZ = "Fizz";
 	public final String BUZZ = "Buzz";
@@ -10,16 +8,16 @@ public class FizzBuzzSolution {
 		return doFizzBuzzLogic(number);
 	}
 
-	private String doFizzBuzzLogic(Integer number){
-    	if(number % 3 ==0){
-    		if(number % 15 ==0){
-    			return FIZZ + BUZZ;
-    		}else{
-    			return FIZZ;
-    		}
-    		else if(number % 5 ==0){
-    			return BUZZ;
-    		}
-    	}
-    }
+	private String doFizzBuzzLogic(Integer number) {
+		if (number % 3 == 0) {
+			if (number % 15 == 0) {
+				return FIZZ + BUZZ;
+			} else {
+				return FIZZ;
+			}
+		} else if (number % 5 == 0) {
+			return BUZZ;
+		}
+		return String.valueOf(number);
+	}
 }
---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,14 @@
+package befaster.solutions.FIZ;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzSolutionTest {
+
+	@Test
+	public void testFizzBuzz() {
+		fail("Not yet implemented");
+	}
+
+}
