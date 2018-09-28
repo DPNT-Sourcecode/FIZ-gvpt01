49afc731850cebdbfbb49c255d77c685358881cb
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 08:57:40 +0000

    Fri Sep 28 08:57:40 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -20,12 +20,15 @@ public class FizzBuzzSolutionTest {
 	@Test
 	public void testSolutionForFizzBuzz() {
 		assertEquals(fizzBuzzSolution.FIZZBUZZ, fizzBuzzSolution.fizzBuzz(15));
+		assertEquals(fizzBuzzSolution.FIZZBUZZ, fizzBuzzSolution.fizzBuzz(30));
+		assertEquals(fizzBuzzSolution.FIZZBUZZ, fizzBuzzSolution.fizzBuzz(45));
 	}
 
 	@Test
 	public void testSolutionForNOTFizzBuzz() {
 		// Numbers 1,2,4,7,8,11 are not divisible by 3 or 5.. This test that the
 		// number passed in is returned as a String
+		
 		assertEquals(Integer.toString(1), fizzBuzzSolution.fizzBuzz(1));
 		assertEquals(Integer.toString(2), fizzBuzzSolution.fizzBuzz(2));
 		assertEquals(Integer.toString(4), fizzBuzzSolution.fizzBuzz(4));
@@ -39,5 +42,6 @@ public class FizzBuzzSolutionTest {
 		assertEquals(Integer.toString(19), fizzBuzzSolution.fizzBuzz(19));
 		assertEquals(Integer.toString(22), fizzBuzzSolution.fizzBuzz(22));
 		assertEquals(Integer.toString(23), fizzBuzzSolution.fizzBuzz(23));
+		
 	}
 }
