8d46f994067774fdbabbd3c3e2c87156545f5eb7
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 10:31:55 +0000

    Fri Sep 28 10:31:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -43,11 +43,7 @@ public class FizzBuzzSolution {
 		}
 		// if number divisible by 5 or contains 5 return BUZZ
 		if (number % 5 == 0 || numberToCheckAsString.contains(number5)) {
-			if(conditionsNotMet){
-				response = response +" "+ BUZZ;
-			}else{
 				response = response + BUZZ;
-			}
 			conditionsNotMet = true;
 		}
 		// Conditions not meet so return number as string
