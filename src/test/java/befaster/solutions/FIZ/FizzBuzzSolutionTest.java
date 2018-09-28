414993f08e4ff23a4a2df1c842305f41de47da3d
sbx_user1059 <sbx_user1059@ip-10-128-137-21.eu-west-2.compute.internal>
2018-09-28 09:55:55 +0000

    Fri Sep 28 09:55:55 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -58,9 +58,9 @@ public class FizzBuzzSolutionTest {
 	@Test
 	public void testFizzBuzzWith5NumberCheckContaining5(){
 		assertEquals(fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(5));
-		assertEquals(fizzBuzzSolution.FIZZBUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(15));
+		assertEquals(fizzBuzzSolution.FIZZ +" " + fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(15));
 		assertEquals(fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(25));
 		assertEquals(fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(35));
-		assertEquals(fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(45));
+		assertEquals(fizzBuzzSolution.FIZZ +" " + fizzBuzzSolution.BUZZ,fizzBuzzSolution.doFizzBuzzWith3or5NumberCheck(30));
 	}
 }
